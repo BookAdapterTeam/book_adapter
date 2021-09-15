3b1c8a2b3c952f185e895de8c34b69e920d383cc
getBoolean <danielwnewville@gmail.com>
2021-09-15 14:19:08 -0500

    init firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:book_adapter/app.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  
+void main() async {
+  await init();
+
   runApp(const ProviderScope(child: MyApp()));
 }
 
+Future<void> init() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+}
 
 
 
