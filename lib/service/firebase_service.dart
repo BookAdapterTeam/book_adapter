2084d8afa59e833d57dfd5e6d7b1020a57f4bf3e
getBoolean <danielwnewville@gmail.com>
2021-09-20 20:25:12 -0500

    Add logIn, signUp, and authStateChange to FirebaseService

--- lib/service/firebase_service.dart
+++ lib/service/firebase_service.dart
@@ -1,10 +1,106 @@
+import 'package:book_adapter/data/book_item.dart';
+import 'package:book_adapter/data/failure.dart';
+import 'package:book_adapter/service/base_firebase_service.dart';
+import 'package:dartz/dartz.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class FirebaseService {
-  FirebaseService();
+/// Provider to easily get access to the [FirebaseService] functions from anywhere in the app
+final firebaseServiceProvider = Provider<FirebaseService>((ref) {
+  final firebaseAuth = FirebaseAuth.instance;
 
-  Future<UserCredential> login({required String email, required String password}) async {
-    final UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(email: email, password: password);
-    return userCredential;
+  return FirebaseService(firebaseAuth);
+});
+
+/// A utility class to handle all Firebase calls
+class FirebaseService extends BaseFirebaseService {
+  FirebaseService(this._firebaseAuth) : super(_firebaseAuth);
+
+  final FirebaseAuth _firebaseAuth;
+
+  // Authentication
+
+  /// Notifies about changes to the user's sign-in state (such as sign-in or
+  /// sign-out).
+  @override
+  Stream<User?> get authStateChange => _firebaseAuth.authStateChanges();
+
+  /// Attempts to sign in a user with the given email address and password.
+  ///
+  /// If successful, it also signs the user in into the app and updates
+  /// the stream [authStateChange]
+  ///
+  /// **Important**: You must enable Email & Password accounts in the Auth
+  /// section of the Firebase console before being able to use them.
+  ///
+  /// Returns an [Either]
+  /// 
+  /// Right [UserCredential] is returned if successful
+  /// 
+  /// Left [Failure] maybe returned with the following error code:
+  /// - **invalid-email**:
+  ///  - Returned if the email address is not valid.
+  /// - **user-disabled**:
+  ///  - Returned if the user corresponding to the given email has been disabled.
+  /// - **user-not-found**:
+  ///  - Returned if there is no user corresponding to the given email.
+  /// - **wrong-password**:
+  ///  - Returned if the password is invalid for the given email, or the account
+  ///    corresponding to the email does not have a password set.
+  @override
+  Future<Either<Failure, UserCredential>> signIn({required String email, required String password}) async {
+    try {
+      final userCredential = await _firebaseAuth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      // Return the data incase the caller needs it
+      return Right(userCredential);
+    } on FirebaseAuthException catch (e) {
+      return Left(FirebaseFailure(e.message ?? 'Login Unsuccessful', e.code));
+    } on Exception catch (_) {
+      return Left(Failure('Unexpected Exception, Could Not Login'));
+    }
+  }
+
+  /// Tries to create a new user account with the given email address and
+  /// password.
+  ///
+  /// Returns an [Either]
+  /// 
+  /// Right [UserCredential] is returned if successful
+  /// 
+  /// Left [Failure] maybe returned with the following error code:
+  /// - **email-already-in-use**:
+  ///  - Returned if there already exists an account with the given email address.
+  /// - **invalid-email**:
+  ///  - Returned if the email address is not valid.
+  /// - **operation-not-allowed**:
+  ///  - Returned if email/password accounts are not enabled. Enable
+  ///    email/password accounts in the Firebase Console, under the Auth tab.
+  /// - **weak-password**:
+  ///  - Returned if the password is not strong enough.
+  @override
+  Future<Either<Failure, UserCredential>> signUp({required String email, required String password}) async {
+    try {
+      final userCredential = await _firebaseAuth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      // Return the data incase the caller needs it
+      return Right(userCredential);
+    } on FirebaseAuthException catch (e) {
+      return Left(FirebaseFailure(e.message ?? 'Signup Not Successful', e.code));
+    } on Exception catch (_) {
+      return Left(Failure('Unexpected Exception, Could Not SignUp'));
+    }
+  }
+
+  /// Signs out the current user.
+  ///
+  /// If successful, it also update the stream [authStateChange]
+  @override
+  Future<void> signOut() async {
+    await _firebaseAuth.signOut();
   }
 }
