4b03266d435a9334538dbebb415ca4212aa0ddef
DNSbhan <dineshbhandari60@gmail.com>
2021-09-25 18:28:56 -0500

    Update login_view.dart

--- lib/authentication/login_view.dart
+++ lib/authentication/login_view.dart
@@ -1,7 +1,8 @@
-import 'package:book_adapter/authentication/login_view_controller.dart';
-import 'package:book_adapter/authentication/register_view.dart';
-import 'package:book_adapter/authentication/reset_password_view.dart';
+
 import 'package:book_adapter/data/failure.dart';
+import 'package:book_adapter/features/auth/login_view_controller.dart';
+import 'package:book_adapter/features/auth/register_view.dart';
+import 'package:book_adapter/features/auth/reset_password_view.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
