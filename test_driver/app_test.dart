f9e9d17edc72902132e5b94febf913ea7b64a1da
getBoolean <danielwnewville@gmail.com>
2021-09-24 21:47:01 -0500

    docs: Update comments for driver

--- test_driver/app_test.dart
+++ test_driver/app_test.dart
@@ -1,16 +1,18 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_driver/flutter_driver.dart';
 import 'package:test/test.dart';
 
 // Launch an emulator, then run the following command
 // `flutter drive --target=test_driver/app.dart`
+
+// Writing integration tests documentation
+// https://flutter.dev/docs/cookbook/testing/integration/introduction#2-add-the-flutter_driver-dependency
 void main() {
   group('BookAdapter App', () {
     // First, define the Finders and use them to locate widgets from the
     // test suite. Note: the Strings provided to the `byValueKey` method must
     // be the same as the Strings we used for the Keys
 
-    // final refreshLoadingIndicatorFinder = find.byValueKey('loading_refresh');
-    // final booksLoadingIndicatorFinder = find.byValueKey('loading_books');
     // final settingsButtonFinder = find.byValueKey('settings');
     // final refreshButtonFinder = find.byValueKey('refresh');
 
@@ -26,29 +28,6 @@ void main() {
       await driver.close();
     });
 
-    // Add tests here, example tests are below
-
-    // test('starts at 0', () async {
-    //   // Use the `driver.getText` method to verify the counter starts at 0.
-    //   expect(await driver.getText(counterTextFinder), "0");
-    // });
-
-    // test('increments the counter', () async {
-    //   // First, tap the button.
-    //   await driver.tap(buttonFinder);
-
-    //   // Then, verify the counter text is incremented by 1.
-    //   expect(await driver.getText(counterTextFinder), "1");
-    // });
-
-    // test('increments the counter during animation', () async {
-    //   await driver.runUnsynchronized(() async {
-    //     // First, tap the button.
-    //     await driver.tap(buttonFinder);
-
-    //     // Then, verify the counter text is incremented by 1.
-    //     expect(await driver.getText(counterTextFinder), "1");
-    //   });
-    // });
+    // TODO: Add tests for navigating app routes (e.g.: Library -> Library Details -> Book Reader)
   });
 }
