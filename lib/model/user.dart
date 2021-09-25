522e40cb68477b407c2dd540ebec087b7e1c4fca
Suman1415 <84302067+Suman1415@users.noreply.github.com>
2021-09-25 12:50:28 -0500

    Active User info

---
+++ lib/model/user.dart
@@ -0,0 +1,16 @@
+class User {
+  final String imagePath;
+  final String name;
+  final String email;
+  final String about;
+  final bool isDarkMode;
+
+  // constructor
+  const User({
+    required this.imagePath,
+    required this.name,
+    required this.email,
+    required this.about,
+    required this.isDarkMode,
+  });
+}
