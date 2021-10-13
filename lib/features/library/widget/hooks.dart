a19138427d1c3290aac141257b2750b9bd306b41
getBoolean <danielwnewville@gmail.com>
2021-10-13 11:49:43 -0500

    Allow setting selection in useDragSelectGridViewController

--- lib/features/library/widget/hooks.dart
+++ lib/features/library/widget/hooks.dart
@@ -4,8 +4,11 @@ import 'package:drag_select_grid_view/drag_select_grid_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
-DragSelectGridViewController useDragSelectGridViewController() {
-  return use(const _DragSelectGridViewControllerHook());
+/// Creates a controller for [DragSelectGridView].
+///
+/// The initial selection is [Selection.empty], unless a different one is provided.
+DragSelectGridViewController useDragSelectGridViewController([Selection? selection]) {
+  return use(_DragSelectGridViewControllerHook(selection));
 }
 
 
