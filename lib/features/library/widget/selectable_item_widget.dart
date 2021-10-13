0b6d8131a3104e6c281d5df8c52fad1536d2f043
Suman1415 <84302067+Suman1415@users.noreply.github.com>
2021-10-13 00:37:18 -0500

    added widgets for selectable items

---
+++ lib/features/library/widget/selectable_item_widget.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class SelectableItemWidget extends StatefulWidget {
+  final String url;
+  final bool isSelected;
+
+  const SelectableItemWidget({
+    Key? key,
+    required this.url,
+    required this.isSelected,
+  }) : super(key: key);
+
+  @override
+  _SelectableItemWidgetState createState() => _SelectableItemWidgetState();
+}
+
+class _SelectableItemWidgetState extends State<SelectableItemWidget>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation<double> scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = AnimationController(
+      value: widget.isSelected ? 1 : 0,
+      duration: kThemeChangeDuration,
+      vsync: this,
+    );
+
+    scaleAnimation = Tween<double>(begin: 1, end: 0.8).animate(CurvedAnimation(
+      parent: controller,
+      curve: Curves.ease,
+    ));
+  }
+
+  @override
+  void didUpdateWidget(SelectableItemWidget oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    if (oldWidget.isSelected != widget.isSelected) {
+      if (widget.isSelected) {
+        controller.forward();
+      } else {
+        controller.reverse();
+      }
+    }
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) => AnimatedBuilder(
+        animation: scaleAnimation,
+        builder: (context, child) => Transform.scale(
+          scale: scaleAnimation.value,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(widget.isSelected ? 80 : 20),
+            child: child,
+          ),
+        ),
+        child: Image.network(widget.url, fit: BoxFit.cover),
+      );
+}
