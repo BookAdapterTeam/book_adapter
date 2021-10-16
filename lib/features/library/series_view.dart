e51282aac1ef1a7ceb7123e47eae7935b5124b9e
getBoolean <danielwnewville@gmail.com>
2021-10-16 17:11:11 -0500

    Disable select in series view

--- lib/features/library/series_view.dart
+++ lib/features/library/series_view.dart
@@ -6,10 +6,11 @@ import 'package:book_adapter/features/library/library_view_controller.dart';
 import 'package:book_adapter/features/library/widgets/item_list_tile_widget.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:implicitly_animated_reorderable_list/implicitly_animated_reorderable_list.dart';
 
-class SeriesView extends ConsumerWidget {
+class SeriesView extends HookConsumerWidget {
   const SeriesView({ Key? key }) : super(key: key);
 
   static const routeName = '/series';
@@ -27,59 +28,23 @@ class SeriesView extends ConsumerWidget {
 
     final imageUrl = series.imageUrl;
 
+    final scrollController = useScrollController();
+
     // ignore: prefer_const_constructors
     return Scaffold(
       // appBar: AppBar(title: const Text('Series'),),
       // body: const Center(child: Text('Books here'),),
       // ignore: prefer_const_constructors
       body: CustomScrollView(
+        controller: scrollController,
         slivers: [
-          SliverAppBar(
-            expandedHeight: 250,
-            flexibleSpace: imageUrl != null ? FlexibleSpaceBar(
-              title: Text(series.title),
-              stretchModes: const <StretchMode>[
-                StretchMode.zoomBackground,
-                StretchMode.blurBackground,
-                StretchMode.fadeTitle,
-              ],
-              background: Stack(
-                fit: StackFit.expand,
-                children: <Widget>[
-                  BackdropFilter(
-                    filter: ImageFilter.blur(sigmaX: 3.0, sigmaY: 3.0),
-                    child: ImageFiltered(
-                      imageFilter: ImageFilter.blur(sigmaX: 4.0, sigmaY: 4.0),
-                      child: CachedNetworkImage(
-                        fit: BoxFit.fitWidth,
-                        imageUrl: imageUrl,
-                        width: 40,
-                      ),  // Widget that is blurred
-                    ),
-                  ),
-                  const DecoratedBox(
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                        begin: Alignment(0.0, 0.5),
-                        end: Alignment.center,
-                        colors: <Color>[
-                          Color(0x60000000),
-                          Color(0x00000000),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ) : null,
-          ),
+          _SliverBackgroundAppBar(imageUrl: imageUrl, series: series),
           SliverImplicitlyAnimatedList<Book>(
             items: books ?? [],
             itemBuilder: (context, animation, item, index) => itemBuilder(context, animation, item, index, books),
             areItemsTheSame: (oldItem, newItem) { 
               return oldItem.id == newItem.id;
             },
-            
           ),
         ],
       ),
@@ -89,6 +54,62 @@ class SeriesView extends ConsumerWidget {
   Widget itemBuilder(BuildContext context, Animation<double> animation, Book item, int index, List<Book>? books) {
     return ItemListTileWidget(
       item: item,
+      disableSelect: true,
+    );
+  }
+}
+
+class _SliverBackgroundAppBar extends StatelessWidget {
+  const _SliverBackgroundAppBar({
+    Key? key,
+    required this.imageUrl,
+    required this.series,
+  }) : super(key: key);
+
+  final String? imageUrl;
+  final Series series;
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      expandedHeight: 250,
+      stretch: true,
+      flexibleSpace: imageUrl != null ? FlexibleSpaceBar(
+        title: Text(series.title),
+        stretchModes: const <StretchMode>[
+          StretchMode.zoomBackground,
+          StretchMode.blurBackground,
+          StretchMode.fadeTitle,
+        ],
+        background: Stack(
+          fit: StackFit.expand,
+          children: <Widget>[
+            BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 3.0, sigmaY: 3.0),
+              child: ImageFiltered(
+                imageFilter: ImageFilter.blur(sigmaX: 4.0, sigmaY: 4.0),
+                child: CachedNetworkImage(
+                  fit: BoxFit.fitWidth,
+                  imageUrl: imageUrl!,
+                  width: 40,
+                ),  // Widget that is blurred
+              ),
+            ),
+            const DecoratedBox(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment(0.0, 0.5),
+                  end: Alignment.center,
+                  colors: <Color>[
+                    Color(0x60000000),
+                    Color(0x00000000),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ) : null,
     );
   }
 }
