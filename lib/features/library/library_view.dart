5f846f22d529dad4da283e2109498d08a9a5bf36
getBoolean <danielwnewville@gmail.com>
2021-10-20 11:38:33 -0500

    Sort collections ignoring case

--- lib/features/library/library_view.dart
+++ lib/features/library/library_view.dart
@@ -105,6 +105,8 @@ class LibraryScrollView extends HookConsumerWidget {
     final appBar = data.isSelecting
             ? isSelectingAppBar
             : notSelectingAppBar;
+    
+    final collections = (data.collections ?? [])..sort((a, b) => a.name.toLowerCase().compareTo(b.name.toLowerCase()));
 
     return CustomScrollView(
       controller: scrollController,
@@ -116,7 +118,7 @@ class LibraryScrollView extends HookConsumerWidget {
         
         // List of collections
         SliverImplicitlyAnimatedList<BookCollection>(
-          items: data.collections ?? [],
+          items: collections,
           areItemsTheSame: (a, b) => a.id == b.id,
           itemBuilder: (context, animation, collection, index) 
               => collectionsBuilder(context, animation, collection, index, scrollController),
