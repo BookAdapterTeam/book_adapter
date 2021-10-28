4158beb74ec047a32a0f5bc4162e35e6faae8ae9
getBoolean <danielwnewville@gmail.com>
2021-10-27 23:06:37 -0500

    Remove unused package

--- lib/features/reader/book_reader_view.dart
+++ lib/features/reader/book_reader_view.dart
@@ -3,7 +3,6 @@ import 'package:book_adapter/features/library/data/book_item.dart';
 import 'package:book_adapter/features/reader/epub_controller.dart';
 import 'package:epub_view/epub_view.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_html/flutter_html.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:logger/logger.dart';
 
