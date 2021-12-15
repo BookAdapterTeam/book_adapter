1ab3272b8a0a8f40a5586d8e3b882afb163bae92
getBoolean <danielwnewville@gmail.com>
2021-12-15 10:03:13 -0600

    Move docs

--- lib/controller/storage_controller.dart
+++ lib/controller/storage_controller.dart
@@ -54,7 +54,50 @@ class StorageController {
     return _read(storageServiceProvider).getAppFilePath(userId);
   }
 
-  
+  /// Add a list of new books
+  /// 
+  /// Get book status
+  ///
+  /// Family StateNotifierProvider `StateNotifierProvider.family<AsyncValue<BookStatus>, Book>`
+  /// - Pass in book id
+  /// - Default value is `AsyncValue.loading()`  // Dont show logo if loading
+  /// - Get File Status with pub package watcher
+  ///
+  ///     - Only update state if it is `AsyncValue.loading(), BookStatus.downloaded, or BookStatus.notDownloaded`
+  ///
+  ///     - Run if `AsyncValue` set to `loading()`
+  ///
+  /// - Manually set `downloading, waiting, uploading, errorUploading, errorDownloading`
+  /// - with methods and callbacks
+  ///
+  /// - Returns type `AsyncValue<BookStatus>`
+  ///
+  /// Use new Dart isolate groups, create a new isolate in the books group for every book
+  /// 
+  /// 0. Add placeholder document first with only filename and upload waiting icon
+  /// 1. Create Isolate 1
+  /// 2. Pass list of book filepaths to Isolate 1
+  /// 3. Isolate 1 uses compute() to create a new isolate in same group for every book to upload
+  ///
+  ///     a. Each compute is passed the book filepath
+  ///
+  ///     b. The following is performed in the new isolate (Parsing and Uploading the Book)
+  ///
+  /// Parsing and Uploading the Book
+  /// 1. Get MD5 and SHA1 of File Contents
+  /// 2. Check Firestore for user books with same MD5 and SHA1
+  ///     -   If book found, stop uploading and show snack bar with message "Book already uploaded",
+  /// 3. Grab Book Cover Image
+  ///     -   If no cover image exists, put null in book document for the cover image url.
+  ///
+  ///         In the app, a default image will be shown included in the assets if image url is null
+  /// 4. Upload Book Cover Image
+  ///     -   Don't upload if null
+  /// 5. Upload Book Document with Cover Image URL, MD5, and SHA1
+  /// 6. Upload Book File with MD5 and SHA1 in metadata
+  Future<void> uploadMultipleBooks() async {
+
+  }
 
   Future<void> downloadBookFile(
     Book book, {
