32f3d4cc76a415bcc9a353bfcb49a2416df4497a
getBoolean <danielwnewville@gmail.com>
2022-05-01 22:37:34 -0500

    create empty PagedHtml widget

--- packages/paged_html/lib/src/paged_html.dart
+++ packages/paged_html/lib/src/paged_html.dart
@@ -1,5 +1,32 @@
+import 'package:flutter/material.dart';
+
 /// A Calculator.
 class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
+}
+
+/// A Widget that displays the html in horizontal or verticle pages
+class PagedHtml extends StatefulWidget {
+  const PagedHtml({
+    Key? key,
+    required this.html,
+    this.scrollDirection = Axis.horizontal,
+  }) : super(key: key);
+
+  final Axis scrollDirection;
+  final String html;
+
+  @override
+  State<PagedHtml> createState() => _PagedHtmlState();
+}
+
+class _PagedHtmlState extends State<PagedHtml> {
+  @override
+  Widget build(BuildContext context) {
+    return PageView.builder(
+      scrollDirection: widget.scrollDirection,
+      itemBuilder: (context, index) => Container(),
+    );
+  }
 }
