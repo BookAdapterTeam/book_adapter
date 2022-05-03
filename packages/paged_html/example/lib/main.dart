4ed9a13ac4cc7044c029f4772e9b95de4961b1cd
getBoolean <danielwnewville@gmail.com>
2022-05-02 22:42:51 -0500

    prototype HtmlPage

--- packages/paged_html/example/lib/main.dart
+++ packages/paged_html/example/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:paged_html/paged_html.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,53 +15,44 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const PagedHtmlExample(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class PagedHtmlExample extends StatelessWidget {
+  const PagedHtmlExample({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      appBar: AppBar(title: const Text('PagedHtml Example')),
+      body: PagedHtml(
+        key: const ValueKey('HtmlReader'),
+        scrollDirection: Axis.horizontal,
+        physics: const PageScrollPhysics(),
+        html: '''
+<div>
+  <h1>PagedHtml</h1>
+  <p>A Widget that displays the html in horizontal or verticle pages</p>
+  <p>
+    <a href="https://pub.dev/packages/paged_html">
+      https://pub.dev/packages/paged_html
+    </a>
+  </p>
+  <p>
+    <a href="https://pub.dev/packages/paged_html#readme">
+      https://pub.dev/packages/paged_html#readme
+    </a>
+  </p>
+  <p>
+    <a href="https://pub.dev/packages/paged_html#example">
+      https://pub.dev/packages/paged_html#example
+    </a>
+  </p>
+</div>
+''' *
+            5,
       ),
     );
   }
--- packages/paged_html/lib/src/paged_html.dart
+++ packages/paged_html/lib/src/paged_html.dart
@@ -1,4 +1,6 @@
+import 'package:boxy/boxy.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_widget_from_html_core/flutter_widget_from_html_core.dart';
 
 /// A Calculator.
 class Calculator {
@@ -11,22 +13,149 @@ class PagedHtml extends StatefulWidget {
   const PagedHtml({
     Key? key,
     required this.html,
+    this.physics,
     this.scrollDirection = Axis.horizontal,
+    this.scrollBehavior,
+    this.pageTurnDuration = const Duration(milliseconds: 300),
+    this.nextPageCurve = Curves.easeInOut,
+    this.previousPageCurve = Curves.easeInOut,
   }) : super(key: key);
 
   final Axis scrollDirection;
   final String html;
+  final ScrollPhysics? physics;
+  final ScrollBehavior? scrollBehavior;
+  final Duration pageTurnDuration;
+  final Curve nextPageCurve;
+  final Curve previousPageCurve;
 
   @override
   State<PagedHtml> createState() => _PagedHtmlState();
 }
 
 class _PagedHtmlState extends State<PagedHtml> {
+  late PageController _pageController;
+  int currentPage = 0;
+
+  @override
+  void initState() {
+    _pageController = PageController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return PageView.builder(
-      scrollDirection: widget.scrollDirection,
-      itemBuilder: (context, index) => Container(),
+    return GestureDetector(
+      onPanUpdate: _handlePanGesture,
+      // onTap: () => _nextPage(),
+      // onDoubleTap: () => _prevPage(),
+      child: PageView.builder(
+        scrollDirection: widget.scrollDirection,
+        controller: _pageController,
+        scrollBehavior: widget.scrollBehavior,
+        physics: widget.physics,
+        onPageChanged: (index) {
+          setState(() {
+            currentPage = index;
+          });
+        },
+        itemBuilder: (context, index) {
+          return HtmlPage(html: widget.html, page: index);
+        },
+      ),
     );
+  }
+
+  Future<void> _nextPage() => _pageController.animateToPage(
+        currentPage + 1,
+        duration: widget.pageTurnDuration,
+        curve: widget.nextPageCurve,
+      );
+
+  Future<void> _prevPage() => _pageController.animateToPage(
+        currentPage - 1,
+        duration: widget.pageTurnDuration,
+        curve: widget.previousPageCurve,
+      );
+
+  void _handlePanGesture(DragUpdateDetails details) {
+    // TODO: Improve gesture handling
+    if (widget.scrollDirection == Axis.horizontal) {
+      if (details.delta.dx > 0) {
+        // right
+        _prevPage();
+      } else {
+        // left
+        _nextPage();
+      }
+    } else {
+      if (details.delta.dy > 0) {
+        // down
+        _prevPage();
+      } else {
+        // up
+        _nextPage();
+      }
+    }
+  }
+}
+
+class HtmlPage extends StatelessWidget {
+  const HtmlPage({
+    Key? key,
+    required this.html,
+    required this.page,
+  }) : super(key: key);
+
+  final String html;
+  final int page;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Flexible(
+          child: CustomBoxy(
+            delegate: _HtmlPageDelegate(html: html),
+            children: const [],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _HtmlPageDelegate extends BoxyDelegate {
+  _HtmlPageDelegate({required this.html});
+
+  final String html;
+
+  @override
+  Size layout() {
+    final htmlWidget = Container(
+      color: Colors.grey,
+      child: HtmlWidget(
+        // '<h1>Hello World</h1>',
+        html,
+        enableCaching: true,
+        renderMode: const ListViewMode(shrinkWrap: true),
+      ),
+    );
+
+    final htmlChild = inflate(htmlWidget, id: #html);
+
+    final actualSize = htmlChild.layout(constraints);
+    // print('Html Height: ${actualSize.height}');
+    // print('Max Height: ${constraints.maxHeight}');
+
+    // if actual height == max height, rebuild with less html
+
+    return actualSize;
   }
 }
